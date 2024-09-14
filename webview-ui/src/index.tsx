72e5fa3dca1d1524e47a376940632799baeab03e
uniunitaro <81953006+uniunitaro@users.noreply.github.com>
2024-09-15 00:19:15 +0900

    feat: タブ切り替え時に再フェチしないように永続化

--- webview-ui/src/index.tsx
+++ webview-ui/src/index.tsx
@@ -1,10 +1,12 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { QueryClient } from '@tanstack/react-query'
+import { PersistQueryClientProvider } from '@tanstack/react-query-persist-client'
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider, createMemoryRouter } from 'react-router-dom'
 import ConnectionSetting from './connection-setting/ConnectionSetting'
 import './index.css'
 import Table from './table/Table'
+import { createVSCodePersister } from './utilities/queryPersister'
 
 // biome-ignore lint/style/noNonNullAssertion: It exists
 const rootElement = document.getElementById('root')!
@@ -32,12 +34,25 @@ const router = createMemoryRouter(
   },
 )
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      gcTime: 1000 * 60 * 60 * 24, // 24時間
+      // staleTimeをセットしないとPersisterのデータを古いと判断して普通にフェッチしてしまう
+      staleTime: 1000 * 60 * 60, // 1時間
+    },
+  },
+})
+
+const persister = createVSCodePersister()
 
 root.render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
+    <PersistQueryClientProvider
+      client={queryClient}
+      persistOptions={{ persister }}
+    >
       <RouterProvider router={router} />
-    </QueryClientProvider>
+    </PersistQueryClientProvider>
   </React.StrictMode>,
 )
