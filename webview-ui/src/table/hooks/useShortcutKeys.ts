0ea16b83b5cd1c973a08425df0b304827895aeec
uniunitaro <81953006+uniunitaro@users.noreply.github.com>
2024-09-29 00:13:33 +0900

    fix: 複数選択時Shiftキー関連バグの修正

--- webview-ui/src/table/hooks/useShortcutKeys.ts
+++ webview-ui/src/table/hooks/useShortcutKeys.ts
@@ -27,12 +27,19 @@ export const useShortcutKeys = ({
       `${Key.Shift}+${Key.ArrowUp}`,
       `${Key.Shift}+${Key.ArrowDown}`,
     ],
-    (event) => {
+    (event, handler) => {
       // デフォルトのスクロール動作を無効化
       event.preventDefault()
 
+      const isShiftPressed = event.shiftKey
+      if (isShiftPressed && !handler.shift) {
+        // Shiftありバージョンとなしバージョンを両方受け取ってしまって選択スピードが速くなるので、
+        // Shiftキーが押されているがショートカットキーにShiftが含まれていない場合は無視
+        return
+      }
+
       moveSelectedCell({
-        direction: event.key === Key.ArrowUp ? 'up' : 'down',
+        direction: handler.keys?.includes('up') ? 'up' : 'down',
         isShiftPressed: event.shiftKey,
       })
     },
@@ -40,23 +47,16 @@ export const useShortcutKeys = ({
     // が、複数行の場合に行を変えるのが不可能になるので要検討
     { enableOnFormTags: true },
   )
-  useHotkeys(
-    [
-      Key.ArrowLeft,
-      Key.ArrowRight,
-      `${Key.Shift}+${Key.ArrowLeft}`,
-      `${Key.Shift}+${Key.ArrowRight}`,
-    ],
-    (event) => {
-      // デフォルトのスクロール動作を無効化
-      event.preventDefault()
 
-      moveSelectedCell({
-        direction: event.key === Key.ArrowLeft ? 'left' : 'right',
-        isShiftPressed: event.shiftKey,
-      })
-    },
-  )
+  useHotkeys([Key.ArrowLeft, Key.ArrowRight], (event, handler) => {
+    // デフォルトのスクロール動作を無効化
+    event.preventDefault()
+
+    moveSelectedCell({
+      direction: handler.keys?.includes('left') ? 'left' : 'right',
+      isShiftPressed: false,
+    })
+  })
 
   useHotkeys(
     [Key.Enter],
